1aa47836ac17bbcaf6058d68256377f8eb75d34e
Alexandros Bantis <ambantis@gmail.com>
2012-05-30 00:43:30 -0700

    Alex's first commit
    Created Initializer, which is a singleton object that starts up the
    system by importing the text files, instantiating the singleton
    University, instantiating the singleton LoginManager (the
    LoginManager's constructor calls LoginManager.displayLoginScreen); it
    also includes a method to save all files and shutdown (if it receives
    a message from the Login Manager that the actor has shutdown. Also
    created the University object, which owns all the managers, and
    vectors for students and courses.

--- sources/com/x460dot10/b/registration/University.java
+++ sources/com/x460dot10/b/registration/University.java
@@ -1,10 +1,69 @@
+/** University.java ---
+ * 
+ */
+
+package com.x460dot10.b.registration;
+
+import java.Util.vector;
 
 public class University {
+     private static University university;
+
+     private LoginManager loginManager;
+     private PasswordManager passwordManager;
+     private SessionManager sessionManager;
+     private RegistrationManager registrationManager;
+     private Vector<Course> courses;
+     private Vector<Student> students;
+
+     // singleton of university
+     private University () {}
+     public static University getInstance()
+     {
+          if (university == null)
+               university = new University();
+          return university;
+     }
 
 
-	public static Boolean addStudent(
-          String firstName, String lastName, String username, String password)
-	{
-		return true;
-	}
+     public Boolean addCourse(Date begin, Date end, String name,
+               String description, int max)
+     {
+          Course newCourse = null;
+          try
+          {
+               newCourse = new Course(begin, end, name, description, max);
+               courses.add(newCourse);
+          }
+          catch (Exception ex)
+          {
+               ex.printStackTrace();
+          }
+          if (newCourse == null)
+               return false;
+          else
+               return true;
+     }
+
+	public Boolean addStudent(String first, String last, String user, 
+               String password)
+     {
+          Student newStudent = null;
+          try
+          {
+               newStudent = new Student(first, last, user, password);
+               students.add(newStudent);
+          }
+          catch (Exception ex)
+          {
+               ex.printStackTrace();
+          }
+          finally 
+          {
+               if (newStudent == null)
+                    return false;
+               else
+                    return true;
+          }
+     }
 }
