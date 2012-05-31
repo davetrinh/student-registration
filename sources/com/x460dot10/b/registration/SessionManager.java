cf876fafebce215dbcf5b0f68e2b5baafac38489
davetrinh <dvutrinh@gmail.com>
2012-05-30 22:59:15 -0700

    Update sources/com/x460dot10/b/registration/SessionManager.java

--- sources/com/x460dot10/b/registration/SessionManager.java
+++ sources/com/x460dot10/b/registration/SessionManager.java
@@ -1,4 +1,29 @@
+import java.util.Iterator;
+import java.util.Vector;
 
 public class SessionManager {
+  
+	public void displayAllCourses(Vector<Course> courses)
+	{
+		Iterator<Course> iter = courses.iterator();	    
+	    Course element = null;
+
+	    while (iter.hasNext()) {
+	    	element = iter.next();
+	    	element.printCourseInfo();
+	    }	   
+		
+	}
+	
+	public void displayStudentCourses(Course course)
+	{
+		
+
+		
+	}
+	public void displayWelcomeMessage()
+	{
+		System.out.println("Hello, welcome to the university!  Please input your user name or password: ");
+	}
 
 }
