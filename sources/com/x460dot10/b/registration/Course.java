9b1ef628be7427e65c3b496a8001a5bebd7fd697
davetrinh <dvutrinh@gmail.com>
2012-05-30 22:51:09 -0700

    Update sources/com/x460dot10/b/registration/Course.java

--- sources/com/x460dot10/b/registration/Course.java
+++ sources/com/x460dot10/b/registration/Course.java
@@ -1,13 +1,15 @@
+import java.sql.Date;
 
 public class Course {
 
-  public Course(String courseName, Integer courseId, String description, Integer maxSeats, Integer currentSeats)
+	public Course(String courseName, Integer courseId, String description, Integer maxSeats, Integer currentSeats, Date start, Date end)
 	{
 		this.m_courseName = courseName;
 		this.m_courseId = courseId;
-		this.m_courseDescription = description;
 		this.m_maxSeats = maxSeats;
 		this.m_currentSeats = currentSeats;
+		this.m_beginDate = start;
+		this.m_endDate = end;
 		
 	}
 	
@@ -64,15 +66,19 @@ public class Course {
 		return m_courseId;
 	}
 	
-	public String getCourseDescription()
+	public void printCourseInfo()
 	{
-		return m_courseDescription;
+		System.out.println("{Course id: " + m_courseId + "} " + "{Course Name: " + m_courseName + "} "
+				+ "{Begin Date: " + m_beginDate.toString() + "} " +  "{End Date: " + m_endDate.toString() + "} "
+				+ "{Max Seats: " + m_maxSeats + "} " + "{Current Seats: " + m_currentSeats + "}" );
+		
 	}
 	
 	private String  m_courseName;
 	private Integer m_courseId;
-	private String  m_courseDescription;
 	private Integer m_maxSeats;
 	private Integer m_currentSeats;
+	private Date    m_beginDate;
+	private Date    m_endDate;
 	
 }
