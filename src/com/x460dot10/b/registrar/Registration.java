0ca218a5fda61135704cb0fe00da4270e2dd1cac
Alexandros Bantis <ambantis@gmail.com>
2012-06-03 10:28:48 -0700

    Samir's first commit of Registration and RegistrationManager

--- src/com/x460dot10/b/registrar/Registration.java
+++ src/com/x460dot10/b/registrar/Registration.java
@@ -1,1 +1,75 @@
 package com.x460dot10.b.registrar;
+
+/**
+ * Registration class
+ *
+ * Registration container
+ *
+ * @author  Samir Bhargava
+ * @version 1.0
+ */
+import java.util.*;
+
+public class Registration {
+  public static int registrationID = 0;
+  private int  studentID = 0;
+  private int  courseID = 0;
+  private Date  registrationDate;
+  private String  memo = "";
+  private int  registrationCount = 0;  
+
+  /**
+   * Registration constructor
+   *
+   * @param  studentID
+   * @param  courseID
+   */
+  public Registration( int studentID, int courseID ) {
+    this.registrationID++;
+    this.registrationDate = new Date();
+    this.studentID = studentID;
+    this.courseID = courseID;
+  }
+    
+  /**
+   * setMemo
+   *
+   * Sets the memo field
+   *
+   * @param  memo
+   */
+  public void setMemo( String memo ) {
+    this.memo = memo;
+  }
+
+  /**
+   * getMemo
+   *
+   * Gets the memo field
+   *
+   * @return  memo
+   */
+  public String getMemo() {
+    return this.memo;
+  }
+
+  /**
+   * addCourse
+   *
+   * Increments the registration counnt
+   *
+   */
+  public void addCourse() {
+    registrationCount++;
+  }
+
+  /**
+   * dropCourse
+   *
+   * Decrements the registration count
+   *
+   */
+  public void dropCourse() {
+    registrationCount--;
+  }
+}
--- src/com/x460dot10/b/registrar/RegistrationManager.java
+++ src/com/x460dot10/b/registrar/RegistrationManager.java
@@ -1,1 +1,123 @@
 package com.x460dot10.b.registrar;
+
+/**
+ *  RegistrationManager class
+ *
+ *  Utility class for manipulation of Registration objects
+ *  
+ *  @author  Samir Bhargava
+ *  @version 1.0
+ *
+ */
+
+import java.util.*;
+
+public class RegistrationManager {  
+  protected Vector<Registration> registrations = new Vector<Registration>();
+
+  /**
+   *  Registration Constructor
+   *
+   *  Adds Registration objects to the RegistrationManager object
+   *
+   *  @param  studentID
+   */
+  public RegistrationManager(int studentID){}
+
+  /**
+   *  addToSchedule method
+   *
+   *  Adds a course to a student's schedule
+   *
+   *  @param  studentID
+   *  @param  courseID
+   *  @return success
+   */
+  public boolean addtoSchedule(int studentID, int courseID){
+    boolean success = true;
+    return success;
+  }
+
+  /**
+   * courseEnrollmentCount method
+   * 
+   * Returns the enrollment count for a given course
+   *
+   * @param  courseID
+   * @return enrollmentCount
+   */
+   public int courseEnrollmentCount(int courseID){
+     int enrollmentCount = 0;
+     return enrollmentCount;
+   }
+
+  /**
+   * dropFromSchedule
+   *
+   * Drops a course from the given student's schedule
+   *
+   * @param  courseID
+   * @return success
+   */
+   public boolean dropFromSchedule(int courseID){
+     boolean success = true;
+     return success;
+   }
+
+  /**
+   * isCourseFull method
+   *
+   * Checks if course is full
+   *
+   * @param  courseID
+   * @return success
+   */
+   public boolean isCourseFull( int courseID ){
+     boolean success = true;
+     return success;
+   }
+
+  /**
+   * isCurrentlyEnrolled method
+   * 
+   * Checks if student is currently enrolled in course
+   *
+   * @param  studentID
+   * @param  courseID
+   * @return success
+   */
+  public boolean isCurrentlyEnrolled(int studentID, int courseID){
+    boolean success = true;
+    return success;
+  }
+
+  /**
+   * showAllCourses method
+   *
+   * @return  allCourses
+   */
+  public String showAllCourses(){
+    String allCourses = "";
+    return allCourses;
+  }
+
+  /**
+   * showMyCourses method
+   *
+   * Displays the courses for a given student
+   *
+   * @param  studentID
+   * @return myCourses
+   */
+  public String showMyCourses(int studentID){
+    String myCourses = "";
+    return myCourses;
+  }
+
+  /**
+   * validateRegistration method
+   *
+   * Validates Registration object
+   */
+  public void validateRegistration(){}
+}
