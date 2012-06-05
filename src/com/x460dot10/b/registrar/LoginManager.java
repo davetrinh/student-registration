5792546d66fcf27841a1087b3f52a8176e2f70f7
Katie Gustafson <noemail@nospam.com>
2012-06-04 21:50:55 -0700

    LoginManager, LoginManagerTest
    Added comments to LoginManager, and changed call to SessionManager.
    Refactored getItemOfUserInput() into two methods, to separate
    functionality of checking that a newly created password is of minimum
    length. Passed student ID to runCurrentStudent(), for SessionManager's
    use.

--- src/com/x460dot10/b/registrar/LoginManager.java
+++ src/com/x460dot10/b/registrar/LoginManager.java
@@ -1,8 +1,16 @@
 package com.x460dot10.b.registrar;
 
-
 import java.util.Scanner;
 
+
+/**
+ * Provides the set of prompts for a user to create a new student account
+ * and to be verified as a student in the university. This verification
+ * allows the student to start a session in the system and use the 
+ * system to make changes to his/her course schedule.
+ * 
+ * @author Katie Gustafson
+ */
 public class LoginManager {
 	
 	private University uni;
@@ -10,6 +18,18 @@ public class LoginManager {
     private static LoginManager loginmgr;
     
  
+    /**
+     * Constructor for singleton of LoginManager, which is called only by
+     * LoginManager.getInstance(). To instantiate a LoginManager object,
+     * <code>LoginManager lm = LoginManager.getInstance();</code>
+     */
+    private LoginManager()
+    {
+		uni = University.getInstance();
+		pwdmgr = PasswordManager.getInstance();
+	}
+    
+    
     public static LoginManager getInstance()
     {
     	if(loginmgr == null)
@@ -19,17 +39,13 @@ public class LoginManager {
     	return loginmgr;
     }
     
-    
-    private LoginManager()
-    {
-		uni = University.getInstance();
-		pwdmgr = PasswordManager.getInstance();
-		
-		this.displayLoginScreen();    
-	}
- 
 
-	private void displayLoginScreen()
+    /**
+     * Directs new students and existing students to different sets of console
+     * prompts.
+     * (This is kind of a confusing method name)
+     */
+	public void displayLoginScreen()
 	{
 		String userAnswer;
 		final String YES_ANSWER = "y";
@@ -49,51 +65,162 @@ public class LoginManager {
 				break;
 			}else if (userAnswer.equalsIgnoreCase(NO_ANSWER))
 			{
-				this.runCurrentStudent();
+				int currentStudentID = promptStudentLogin();
+				this.runCurrentStudent(currentStudentID);
 				break;
 			}
 		}
 	}
 
-	
-	private void runCurrentStudent()
+
+	/**
+	 * Prompts user for student username and password, and uses those two
+	 * parameters to get the ID of the student logging in.
+	 * 
+	 * @return	student ID of the student who logged in
+	 */
+	private int promptStudentLogin()
 	{
-		//starts the process for a current student logging in
-		//ssionManager sm = new SessionManager();
+		int studentID;
+		
+		String username; 
+		String password;
+		
+		Boolean loginAttempted = false;
+		
+		do
+		{
+			if (loginAttempted)
+			{
+				System.out.println("That username and password do not match a student record in " +
+						"the system. Please try again.");
+			}
+			
+			username = this.getItemOfUserInput("Username");
+			password = this.getItemOfUserInput("Password");
+			studentID = pwdmgr.login(username, password);
+		
+			loginAttempted = true;
+		} while (!uni.hasStudent(studentID));
+	
+		return studentID;
+	}
+	
+
+	/**
+	 * Once the student is logged in, the system passes the
+	 * student to the session manager so s/he can use her online
+	 * account.
+	 * 
+	 * @param studentID
+	 */
+	private void runCurrentStudent(int studentID)
+	{
+		String studentName = "";
+		
+		//pretty much need a University.getStudent(studentID) method so
+		//I can either provide sessionManager.INitialize with student name,
+		//or SessionManager can get student name from University by using
+		//University.getStudent(studentID)
+		
+		SessionManager sm = SessionManager.getInstance();
+		sm.initialize(studentName, studentID); //!! studentName is blank right now
+		
+		//might want to pass the student ID to displayMainMenu()
+		//unless we end up using a student-like singleton object
+		//to store the student 
+		sm.displayMainMenu();
 	}
 
-	
+
+	/**
+	 * Gets the new student's information (e.g., name, chosen username, 
+	 * password) and adds the new student's record to the University.
+	 * Then logs the new student into the system.
+	 */
 	private void runNewStudent()
 	{		
 		String firstName = "";
 		String lastName = "";
 		String username = "";
 		String password = "";
+		int studentID;
 		
 		Boolean studentAddedToUniversity = false;
 		
 		while (!studentAddedToUniversity)
 		{	
-			firstName = this.getItemOfUserInput("First name"); 
-			lastName = this.getItemOfUserInput("Last name");
-			username = this.getItemOfUserInput("Username");
-			password = this.getItemOfUserInput("Password");
+			firstName = this.getItemOfNewStudentInfo("First name"); 
+			lastName = this.getItemOfNewStudentInfo("Last name");
+			username = this.getItemOfNewStudentInfo("Username");
+			password = this.getItemOfNewStudentInfo("Password");
 					
 			studentAddedToUniversity = uni.addStudent(firstName, lastName, username, password);
 			
 			if (!studentAddedToUniversity)
 			{
-				System.out.println("There was a problem adding your student record. Please enter your information again.");
+				System.out.println("There was a problem adding your student record. " +
+									"Please enter your information again.");
 			}
 		}
 		
-		runCurrentStudent();
+		studentID = pwdmgr.login(username, password);
+		runCurrentStudent(studentID);
 	}
 
 	
-	private String getItemOfUserInput(String itemTitle)
+	/**
+	 * Prompts user through the console for a particular item of input used for
+	 * creating new student accounts, and checks and returns the user's 
+	 * response.
+	 * In the case of a password input, this requires that the password be
+	 * of minimum length.
+	 * 
+	 * @param itemTitle	The title that will appear in the prompt, e.g., 
+	 * 					"First name" or "Username" or "Password"
+	 * @return			The user's response to the prompt
+	 */
+	private String getItemOfNewStudentInfo(String itemTitle)
 	{
 		final String ITEM_TITLE_PASSWORD = "password";
+		
+		String userInput;
+		String itemValue = "";
+		
+		do
+		{
+			userInput = getItemOfUserInput(itemTitle);
+			
+			if (itemTitle.equalsIgnoreCase(ITEM_TITLE_PASSWORD))
+			{
+				if (pwdmgr.isMinLength(userInput))
+				{
+					itemValue = userInput;
+				}else
+				{
+					System.out.println("The password you chose is too short. Please try again.");
+				}
+			}
+			else
+			{
+				itemValue = userInput;
+			}
+		} while(itemValue.length() < 1);
+		
+		return itemValue;
+	}
+	
+	
+	/**
+	 * Prompts user through the console for a particular item of input, and 
+	 * returns the user's response.
+	 * 
+	 * @param itemTitle	The title that will appear in the prompt, e.g., 
+	 * 					"First name" or "Username" or "Password"
+	 * @return			The user's response to the prompt
+	 */
+	private String getItemOfUserInput(String itemTitle)
+	{
 		Scanner keyboardInput = new Scanner(System.in);
 		
 		String itemValue = "";
@@ -104,17 +231,7 @@ public class LoginManager {
 			System.out.print(itemTitle + ": ");
 			userInput = keyboardInput.next();
 	
-			if (itemTitle.equalsIgnoreCase(ITEM_TITLE_PASSWORD))
-			{
-				if (pwdmgr.isMinLength(userInput))
-				{
-					itemValue = userInput;
-				}else
-				{
-					System.out.println("The password you chose is too short.");
-				}
-			}
-			else if (userInput.length() > 0)
+			if (userInput.length() > 0)
 			{
 				itemValue = userInput;
 			}
--- test/com/x460dot10/b/test/LoginManagerTest.java
+++ test/com/x460dot10/b/test/LoginManagerTest.java
@@ -17,4 +17,31 @@ public class LoginManagerTest {
 		assertEquals(lm1, lm2);	
 	}
 
+	
+	@Test
+	public void testDisplayLoginScreen()
+	{
+		LoginManager lm = LoginManager.getInstance();
+		
+		//right now, this just runs this method so we can test it by using it. 
+		//won't work until University.addStudent works.
+		lm.displayLoginScreen();
+	}
+	
+	@Test
+	public void testRunCurrentStudent()
+	{
+		//add students to uni
+		//try to log in as a student
+		
+		University uni;
+		uni = University.getInstance();
+		uni.addStudent("May", "Juin", "cornflower", "blues");
+		
+		PasswordManager pm = PasswordManager.getInstance();
+		int studentID = pm.login("cornflower", "blues");
+		
+		System.out.println("student ID: " + studentID);
+		//will want to call displayLoginScreen to test whether this student can log in, once added. ?
+	}
 }
